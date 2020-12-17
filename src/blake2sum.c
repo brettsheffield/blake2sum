7be849f8e91e08dc3787df94a9da1853495c79b9
Brett Sheffield <brett@gladserv.com>
2020-12-17 17:53:50 +0100

    hash commandline args
    if commandline args are present, hash these instead of reading from
    stdin

--- src/blake2sum.c
+++ src/blake2sum.c
@@ -7,24 +7,33 @@
 #define HASHSIZE crypto_generichash_BYTES
 #define HEXLEN HASHSIZE * 2 + 1
 
-int main(void)
+int main(int argc, char *argv[])
 {
 	unsigned char hash[HASHSIZE];
 	char hex[HEXLEN];
 	char *s;
 	size_t byt;
 	int pagesz;
-	pagesz = getpagesize();
-	s = malloc(pagesz);
-	if (!s) return ENOMEM;
 	crypto_generichash_state state;
+	if (argc == 1) {
+		pagesz = getpagesize();
+		s = malloc(pagesz);
+		if (!s) return ENOMEM;
+	}
 	crypto_generichash_init(&state, NULL, 0, HASHSIZE);
-	while ((byt = fread(s, 1, pagesz, stdin))) {
-		crypto_generichash_update(&state, (unsigned char *)s, byt);
+	if (argc == 1) {
+		while ((byt = fread(s, 1, pagesz, stdin))) {
+			crypto_generichash_update(&state, (unsigned char *)s, byt);
+		}
+	}
+	else if (argc > 1) {
+		for (int i = 1; i < argc; i++) {
+			crypto_generichash_update(&state, (unsigned char *)argv[i], strlen(argv[i]));
+		}
 	}
 	crypto_generichash_final(&state, hash, HASHSIZE);
 	sodium_bin2hex(hex, HEXLEN, hash, sizeof hash);
 	puts(hex);
-	free(s);
+	if (argc == 1) free(s);
 	return ferror(stdin);
 }
